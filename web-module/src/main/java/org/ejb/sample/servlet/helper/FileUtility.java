571538b8e4e36a2ed2ca2512236074aed8ed0c25
Sridhar <sridharb.in@gmail.com>
2021-03-19 12:01:57 +0530

    Fix for remediation :tesestst

--- web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
+++ web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
@@ -2,17 +2,13 @@ package org.ejb.sample.servlet.helper;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import com.amazonaws.auth.*;
 
 public class FileUtility {
 
-        void generateReportFile(String content) throws IOException {
-
-            FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
-            fileWriter.write(content);
-            fileWriter.close();
-        }
-
+    void generateReportFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
+        fileWriter.write(content);
+        fileWriter.close();
+    }
 }
-
-
-
